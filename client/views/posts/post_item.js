f8fe256cdf325987b70474fc0914ead0171d08d7
lisandro-montano <lisandro@itivitykids.com>
2014-09-05 10:20:06 -0400

    Added re-ordering and new posts animation

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,12 +1,17 @@
+var POST_HEIGHT = 80;
+var Positions = new Meteor.Collection(null);
+
 Template.postItem.helpers({
   ownPost: function() {
     return this.userId == Meteor.userId();
   },
+  
   domain: function() {
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
   },
+  
   upvotedClass: function() {
     var userId = Meteor.userId();
     if (userId && !_.include(this.upvoters, userId)) {
@@ -14,7 +19,26 @@ Template.postItem.helpers({
     } else {
       return 'disabled';
     }
+   },
+  
+  attributes: function() {
+    var post = _.extend({}, Positions.findOne({postId: this._id}), this);
+    var newPosition = post._rank * POST_HEIGHT;
+    var attributes = {};
+    if (_.isUndefined(post.position)) {
+    attributes.class = 'post invisible';
+    } else {
+      var offset = post.position - newPosition;      
+      attributes.style = "top: " + offset + "px";
+      if (offset === 0)
+        attributes.class = "post animate"
+    }
+    Meteor.setTimeout(function() {
+      Positions.upsert({postId: post._id}, {$set: {position: newPosition}})
+    });
+    return attributes;
   }
+
 });
 Template.postItem.events({
   'click .upvote': function(e) {
